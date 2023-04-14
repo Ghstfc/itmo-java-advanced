4f3c49ffa944b6eb9c6b8cc1ae6b9548d14af193
Ghstfc <maks-merkulov-2017@mail.ru>
2023-04-14 13:48:16 +0300

    Added 8 hw and fixed 7

--- java-solutions/info/kgeorgiy/ja/merkulov/concurrent/IterativeParallelism.java
+++ java-solutions/info/kgeorgiy/ja/merkulov/concurrent/IterativeParallelism.java
@@ -1,97 +1,63 @@
 package info.kgeorgiy.ja.merkulov.concurrent;
 
 import info.kgeorgiy.java.advanced.concurrent.ScalarIP;
+import info.kgeorgiy.java.advanced.mapper.ParallelMapper;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class IterativeParallelism implements ScalarIP {
 
+    private final ParallelMapper parallelMapper;
 
-    private <T> T comparingImpl(int threadsNum, List<? extends T> values, Function<List<? extends T>, T> calculationOnParts,
-                                Function<List<? extends T>, T> resultingCalculation) throws InterruptedException {
-        List<List<? extends T>> parts = partition(threadsNum, values);
-
-        List<Thread> threads = new ArrayList<>();
-        // shared list
-        List<T> result = new ArrayList<>();
-
-        mainCalc(calculationOnParts, parts, threads, result);
-
-        // interruption checking
-        interruptionChecking(threads);
-
-        return resultingCalculation.apply(result);
+    public IterativeParallelism(ParallelMapper parallelMapper) {
+        this.parallelMapper = parallelMapper;
     }
 
-    private <T> boolean matchingImpl(int threadsNum, List<? extends T> values, Function<List<? extends T>, Boolean> calculationOnParts,
-                                     Function<List<? extends Boolean>, Boolean> resultingCalculation) throws InterruptedException {
-
-        List<List<? extends T>> parts = partition(threadsNum, values);
-
-        List<Thread> threads = new ArrayList<>();
-        // shared list
-        List<Boolean> result = new ArrayList<>();
-
-        mainCalc(calculationOnParts, parts, threads, result);
-
-        // interruption checking
-        interruptionChecking(threads);
-
-        return resultingCalculation.apply(result);
+    public IterativeParallelism() {
+        this.parallelMapper = null;
     }
 
-    private void interruptionChecking(List<Thread> threads) throws InterruptedException {
-        for (Thread thread : threads) {
-            if (thread.isInterrupted())
-                throw new InterruptedException("Thread №" + thread.getId() + " was interrupted by unknown problem");
+    private <T, R> R mainImpl(int threadsNum, List<? extends T> values, Function<Stream<? extends T>, R> calculationOnParts,
+                              Function<List<? extends R>, R> resultingCalculation) throws InterruptedException {
+        List<R> result = new ArrayList<>();
+        List<Stream<? extends T>> parts = partition(threadsNum, values);
+        if (parallelMapper == null) {
+            List<Thread> threads = new ArrayList<>();
+            // shared list
+
+            mainCalc(calculationOnParts, parts, threads, result);
+        } else {
+            result = parallelMapper.map(calculationOnParts, parts);
         }
-    }
-
-    private <T> Integer countImpl(int threadsNum, List<? extends T> values,
-                                  Function<List<? extends T>, Long> calculationOnParts,
-                                  Function<List<? extends Long>, Integer> resultingCalculation)
-            throws InterruptedException {
-// :NOTE: обощить
-
-        List<List<? extends T>> parts = partition(threadsNum, values);
-
-        List<Thread> threads = new ArrayList<>();
-        // shared list
-        List<Long> result = new ArrayList<>();
-
-        mainCalc(calculationOnParts, parts, threads, result);
-
-        // interruption checking
-        interruptionChecking(threads);
-
         return resultingCalculation.apply(result);
     }
 
 
-    private <T> List<List<? extends T>> partition(int threadsNum, List<? extends T> values) {
-        List<List<? extends T>> parts = new ArrayList<>();
+    private <T> List<Stream<? extends T>> partition(int threadsNum, List<? extends T> values) {
+        List<Stream<? extends T>> parts = new ArrayList<>();
         int size = values.size();
         int partitionSize = size / threadsNum;
         int remainder = size % threadsNum;
         for (int i = 0; i < size; i += partitionSize) {
-            if (size - (i + partitionSize) == remainder) {
-                parts.add(values.subList(i, i + partitionSize + remainder));
-// :NOTE: равномернее распределить задачи
-                break;
+            if (remainder > 0) {
+                parts.add(values.subList(i, i + partitionSize + 1).stream());
+                remainder--;
+                i++;
             } else {
-                parts.add(values.subList(i, i + partitionSize));
+                parts.add(values.subList(i, i + partitionSize).stream());
             }
         }
         return parts;
     }
 
 
-    private <T, R> void mainCalc(Function<List<? extends T>, R> calculationOnParts, List<List<? extends T>> parts,
-                                 List<Thread> threads, List<R> result) {
+    private <T, R> void mainCalc(Function<Stream<? extends T>, R> calculationOnParts, List<Stream<? extends T>> parts,
+                                 List<Thread> threads, List<R> result) throws InterruptedException {
         for (int i = 0; i < parts.size(); i++) {
             result.add(null);
         }
@@ -100,46 +66,36 @@ public class IterativeParallelism implements ScalarIP {
             threads.add(new Thread(() -> result.set(index, calculationOnParts.apply(parts.get(index)))));
             threads.get(index).start();
         }
-        // waiting for all processes ending
-        boolean isProcessing;
-        do {
 
-            isProcessing = false;
-            for (Thread t : threads) {
-//                t.join();
-                // :NOTE:
-                if (t.isAlive()) {
-                    isProcessing = true;
-                    break;
-                }
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                throw new InterruptedException("Some threads was interrupted by unknown problem " + e.getMessage());
             }
+        }
 
-        } while (isProcessing);
     }
 
 
     @Override
     public <T> T maximum(int threadsNum, List<? extends T> values, Comparator<? super T> comparator) throws InterruptedException {
-        return comparingImpl(threadsNum, values,
-                list -> list.stream().max(comparator).get(),
+        return mainImpl(threadsNum, values,
+                list -> list.max(comparator).get(),
                 list -> list.stream().max(comparator).get()
         );
     }
 
     @Override
     public <T> T minimum(int threadsNum, List<? extends T> values, Comparator<? super T> comparator) throws InterruptedException {
-      // :NOTE:  можно переиспользовать
-        return comparingImpl(threadsNum, values,
-                list -> list.stream().min(comparator).get(),
-                list -> list.stream().min(comparator).get()
-        );
+        return maximum(threadsNum, values, comparator.reversed());
     }
 
 
     @Override
     public <T> boolean all(int threadsNum, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
-        return matchingImpl(threadsNum, values,
-                list -> list.stream().allMatch(predicate),
+        return mainImpl(threadsNum, values,
+                list -> list.allMatch(predicate),
                 booleans -> booleans.stream().allMatch(Boolean::booleanValue)
         );
     }
@@ -147,18 +103,17 @@ public class IterativeParallelism implements ScalarIP {
 
     @Override
     public <T> boolean any(int threadsNum, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
-        return matchingImpl(threadsNum, values,
-                list -> list.stream().anyMatch(predicate),
+        return mainImpl(threadsNum, values,
+                list -> list.anyMatch(predicate),
                 booleans -> booleans.stream().anyMatch(Boolean::booleanValue)
         );
     }
 
     @Override
     public <T> int count(int threadsNum, List<? extends T> values, Predicate<? super T> predicate) throws InterruptedException {
-        // :NOTE: сразу в ...impl получать стрим, а не лист
-        return countImpl(threadsNum, values,
-                list -> list.stream().filter(predicate).count(),
-                longs -> longs.stream().mapToInt(Long::intValue).sum()
+        return mainImpl(threadsNum, values,
+                list -> (int) list.filter(predicate).count(),
+                longs -> longs.stream().mapToInt(Integer::intValue).sum()
         );
     }
 }
---
+++ java-solutions/info/kgeorgiy/ja/merkulov/concurrent/ParallelMapperImpl.java
@@ -0,0 +1,159 @@
+package info.kgeorgiy.ja.merkulov.concurrent;
+
+import info.kgeorgiy.java.advanced.mapper.ParallelMapper;
+
+import java.util.*;
+import java.util.function.Function;
+
+public class ParallelMapperImpl implements ParallelMapper {
+
+    private final List<Thread> threads;
+    private final Queue<Runnable> queue;
+//    private final List<Object> result;
+//    private int finalNumber;
+//    private int currentNumber;
+
+
+    private static class Result<T> {
+        private final List<T> result;
+        private final int finalNumber;
+        private int currentNumber;
+
+        private Result(int number) {
+            this.finalNumber = number;
+            this.result = new ArrayList<>(number);
+            for (int i = 0; i < number; i++) {
+                result.add(null);
+            }
+            this.currentNumber = 0;
+        }
+
+        public void setter(int index, T object) {
+            synchronized (result) {
+                result.set(index, object);
+                currentNumber++;
+                result.notifyAll();
+            }
+        }
+
+        public List<T> asList() throws InterruptedException {
+            List<T> res;
+            synchronized (result) {
+                while (currentNumber != finalNumber) {
+                    result.wait();
+                }
+                res = new ArrayList<>(result);
+                result.notifyAll();
+            }
+            return res;
+        }
+    }
+
+
+    public ParallelMapperImpl(int threadsNum) throws InterruptedException {
+        if (threadsNum <= 0) {
+            throw new InterruptedException("incorrect number of threads");
+        }
+        threads = new ArrayList<>();
+        queue = new ArrayDeque<>();
+//        result = new ArrayList<>();
+        for (int i = 0; i < threadsNum; i++) {
+            threads.add(new Thread(() -> {
+                try {
+                    while (!Thread.interrupted()) {
+                        taskRunner();
+                    }
+                } catch (InterruptedException ignored) {
+                } finally {
+                    Thread.currentThread().interrupt();
+                }
+            }));
+            threads.get(i).start();
+        }
+    }
+
+//    private void initRes(int number) {
+//        for (int i = 0; i < number; i++) {
+//            result.add(null);
+//        }
+//    }
+
+
+    @Override
+    public <T, R> List<R> map(Function<? super T, ? extends R> f, List<? extends T> args) throws InterruptedException {
+        Result<R> result = new Result<>(args.size());
+//        initRes(args.size());
+//        finalNumber = args.size();
+        for (int i = 0; i < args.size(); i++) {
+            final int index = i;
+            addQueue(() -> result.setter(index, f.apply(args.get(index))));
+//            addQueue(() -> set(index, f.apply(args.get(index))));
+        }
+        return result.asList();
+    }
+//
+//    private <R> List<R> asList() throws InterruptedException {
+//        List<R> res;
+//        synchronized (result) {
+//            while (currentNumber != finalNumber) {
+//                result.wait();
+//            }
+//            res = new ArrayList<>((Collection<? extends R>) result);
+//            result.notifyAll();
+//        }
+//        return res;
+//    }
+//
+//    private <R> void set(int index, R object) {
+//        synchronized (result) {
+//            result.set(index, object);
+//            currentNumber++;
+//            result.notifyAll();
+//        }
+//    }
+
+
+    private void taskRunner() throws InterruptedException {
+        Runnable runnable;
+        synchronized (queue) {
+            while (queue.isEmpty()) {
+                queue.wait();
+            }
+            runnable = pollQueue();
+            queue.notifyAll();
+        }
+        runnable.run();
+    }
+
+    private void addQueue(Runnable task) {
+        synchronized (queue) {
+            queue.add(task);
+            queue.notifyAll();
+        }
+    }
+
+    private Runnable pollQueue() {
+        Runnable runnable = null;
+        synchronized (queue) {
+            if (!queue.isEmpty()) {
+                runnable = queue.poll();
+            }
+            queue.notifyAll();
+        }
+        return runnable;
+    }
+
+
+    @Override
+    public void close() {
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException ignored) {
+            }
+        }
+    }
+}
